cc17125a6f88ec0263fbe5dd51e20efdaa7a9482
lgb789 <lgb789@126.com>
2016-08-15 14:29:02 +0800

    modify button

--- LGBViewCreate/Classes/UIButton+lgb_button.h
+++ LGBViewCreate/Classes/UIButton+lgb_button.h
@@ -10,19 +10,116 @@
 
 @interface UIButton (lgb_button)
 
+/**
+ *  创建 button
+ *
+ *  @param title                 标题
+ *  @param titleColor            常态颜色
+ *  @param titleHighlightedColor 事件颜色
+ *  @param font                  字体
+ *  @param state                 状态
+ *  @return button
+ */
++ (UIButton*)lgb_buttonWithTitle:(NSString*)title
+                      titleColor:(UIColor*)titleColor
+                 titleEventColor:(UIColor*)titleEventColor
+                       titleFont:(UIFont*)font
+                           state:(UIControlState)state;
+
+/**
+ *  创建 button
+ *
+ *  @param title                 标题
+ *  @param titleColor            常态颜色
+ *  @param titleHighlightedColor 高亮颜色
+ *  @param font                  字体
+ *
+ *  @return button
+ */
 + (UIButton*)lgb_buttonWithTitle:(NSString*)title
                       titleColor:(UIColor*)titleColor
            titleHighlightedColor:(UIColor*)titleHighlightedColor
                        titleFont:(UIFont*)font;
 
+/**
+ *  创建 button
+ *
+ *  @param title                 标题
+ *  @param titleColor            常态颜色
+ *  @param titleHighlightedColor 选中颜色
+ *  @param font                  字体
+ *
+ *  @return button
+ */
++ (UIButton*)lgb_buttonWithTitle:(NSString*)title
+                      titleColor:(UIColor*)titleColor
+              titleSelectedColor:(UIColor*)titleSelectedColor
+                       titleFont:(UIFont*)font;
+
+
+/**
+ *  创建 button
+ *
+ *  @param image      常态图片
+ *  @param eventImage 状态图片
+ *  @param state      状态
+ *
+ *  @return button
+ */
++ (UIButton*)lgb_buttonWithImage:(UIImage*)image eventImage:(UIImage*)eventImage state:(UIControlState)state;
+
+
+/**
+ *  创建 button
+ *
+ *  @param image             常态图片
+ *  @param highlightedImage  高亮图片
+ *
+ *  @return button
+ */
 + (UIButton*)lgb_buttonWithImage:(UIImage*)image highlightedImage:(UIImage*)highlightedImage;
 
+/**
+ *  创建 button
+ *
+ *  @param image         常态图片
+ *  @param selectedImage 选中图片
+ *
+ *  @return button
+ */
 + (UIButton*)lgb_buttonWithImage:(UIImage*)image selectedImage:(UIImage*)selectedImage;
 
 
+/**
+ *  设置 button 背景图片
+ *
+ *  @param image      常态图片
+ *  @param eventImage 状态图片
+ *  @param state      状态
+ */
+- (void)lgb_setBackgroundImage:(UIImage*)image eventImage:(UIImage*)eventImage state:(UIControlState)state;
 
+/**
+ *  设置 button 背景图片
+ *
+ *  @param image            常态图片
+ *  @param highlightedImage 高亮图片
+ */
 - (void)lgb_setBackgroundImage:(UIImage*)image highlightedImage:(UIImage*)highlightedImage;
 
+/**
+ *  设置 button 背景图片
+ *
+ *  @param image            常态图片
+ *  @param highlightedImage 选中图片
+ */
+- (void)lgb_setBackgroundImage:(UIImage*)image selectedImage:(UIImage*)selectedImage;
+
+/**
+ *  设置 button 事件
+ *
+ *  @param action 事件
+ */
 - (void)lgb_setAction:(void (^) (void))action;
 
 @end
--- LGBViewCreate/Classes/UIButton+lgb_button.m
+++ LGBViewCreate/Classes/UIButton+lgb_button.m
@@ -14,53 +14,88 @@
 //class methods
 + (UIButton*)lgb_buttonWithTitle:(NSString*)title
                       titleColor:(UIColor*)titleColor
-           titleHighlightedColor:(UIColor*)titleHighlightedColor
+                 titleEventColor:(UIColor*)titleEventColor
                        titleFont:(UIFont*)font
+                           state:(UIControlState)state
 {
     UIButton* bt = [UIButton buttonWithType:UIButtonTypeCustom];
     [bt setTitle:title forState:UIControlStateNormal];
     [bt setTitleColor:titleColor forState:UIControlStateNormal];
-    if (titleHighlightedColor)
+    if (titleEventColor)
     {
-        [bt setTitleColor:titleHighlightedColor forState:UIControlStateHighlighted];
+        [bt setTitleColor:titleEventColor forState:state];
     }
     bt.titleLabel.font = font;
+    
+    return bt;
+}
 
+
++ (UIButton*)lgb_buttonWithTitle:(NSString*)title
+                      titleColor:(UIColor*)titleColor
+           titleHighlightedColor:(UIColor*)titleHighlightedColor
+                       titleFont:(UIFont*)font
+{
+    UIButton* bt = [self lgb_buttonWithTitle:title titleColor:titleColor titleEventColor:titleHighlightedColor titleFont:font state:UIControlStateHighlighted];
+
+    return bt;
+}
+
++ (UIButton*)lgb_buttonWithTitle:(NSString*)title
+                      titleColor:(UIColor*)titleColor
+              titleSelectedColor:(UIColor*)titleSelectedColor
+                       titleFont:(UIFont*)font
+{
+    UIButton* bt = [self lgb_buttonWithTitle:title titleColor:titleColor titleEventColor:titleSelectedColor titleFont:font state:UIControlStateSelected];
+    
+    return bt;
+}
+
++ (UIButton*)lgb_buttonWithImage:(UIImage*)image eventImage:(UIImage*)eventImage state:(UIControlState)state
+{
+    UIButton* bt = [UIButton buttonWithType:UIButtonTypeCustom];
+    [bt setImage:image forState:UIControlStateNormal];
+    if (eventImage)
+    {
+        [bt setImage:eventImage forState:state];
+    }
+    
     return bt;
 }
 
 + (UIButton*)lgb_buttonWithImage:(UIImage*)image highlightedImage:(UIImage*)highlightedImage
 {
-    UIButton* bt = [UIButton buttonWithType:UIButtonTypeCustom];
-    [bt setImage:image forState:UIControlStateNormal];
-    if (highlightedImage)
-    {
-        [bt setImage:highlightedImage forState:UIControlStateHighlighted];
-    }
+    UIButton* bt = [self lgb_buttonWithImage:image eventImage:highlightedImage state:UIControlStateHighlighted];
 
     return bt;
 }
 
 + (UIButton*)lgb_buttonWithImage:(UIImage*)image selectedImage:(UIImage*)selectedImage
 {
-    UIButton* bt = [UIButton buttonWithType:UIButtonTypeCustom];
-    [bt setImage:image forState:UIControlStateNormal];
-    if (selectedImage)
-    {
-        [bt setImage:selectedImage forState:UIControlStateSelected];
-    }
+    UIButton* bt = [self lgb_buttonWithImage:image eventImage:selectedImage state:UIControlStateSelected];
     
     return bt;
 }
 
 //object methods
-- (void)lgb_setBackgroundImage:(UIImage*)image highlightedImage:(UIImage*)highlightedImage
+
+- (void)lgb_setBackgroundImage:(UIImage*)image eventImage:(UIImage*)eventImage state:(UIControlState)state
 {
     [self setBackgroundImage:image forState:UIControlStateNormal];
-    if (highlightedImage)
+    if (eventImage)
     {
-        [self setBackgroundImage:highlightedImage forState:UIControlStateHighlighted];
+        [self setBackgroundImage:eventImage forState:state];
     }
+}
+
+- (void)lgb_setBackgroundImage:(UIImage*)image highlightedImage:(UIImage*)highlightedImage
+{
+    [self lgb_setBackgroundImage:image eventImage:highlightedImage state:UIControlStateHighlighted];
+}
+
+- (void)lgb_setBackgroundImage:(UIImage*)image selectedImage:(UIImage*)selectedImage
+{
+    [self lgb_setBackgroundImage:image eventImage:selectedImage state:UIControlStateSelected];
 }
 
 - (void)lgb_setAction:(void (^) (void))action
